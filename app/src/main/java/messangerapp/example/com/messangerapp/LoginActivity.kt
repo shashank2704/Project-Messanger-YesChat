e9fd8f9d2dc9e3122d77cfb0bb92ba8d481c9265
shashank2704 <47267540+shashank2704@users.noreply.github.com>
2020-06-04 20:05:10 +0530

    Database

--- app/src/main/java/messangerapp/example/com/messangerapp/LoginActivity.kt
+++ app/src/main/java/messangerapp/example/com/messangerapp/LoginActivity.kt
@@ -3,10 +3,18 @@ package messangerapp.example.com.messangerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity()
 {
+    private  lateinit var mAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -20,6 +28,45 @@ class LoginActivity : AppCompatActivity()
             val intent= Intent(this@LoginActivity, WelcomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        mAuth= FirebaseAuth.getInstance()
+
+        login_btn.setOnClickListener {
+            loginUser()
+        }
+
+
+    }
+
+    private fun loginUser() {
+        val email:String=email_login.text.toString()
+        val password:String=password_login.text.toString()
+
+        if(email=="")
+        {
+            Toast.makeText(this@LoginActivity,"Please Write Email.", Toast.LENGTH_LONG).show()
+        }
+        else if(password=="")
+        {
+            Toast.makeText(this@LoginActivity,"Please Write Password.", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                task ->
+                if(task.isSuccessful)
+                {
+                    val intent=Intent(this@LoginActivity, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+                }
+                else
+                {
+                    Toast.makeText(this@LoginActivity,"Please Check Details: "+ task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
